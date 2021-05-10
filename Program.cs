4a78dc59ca494523c4ca659e2fe7461056ac5b7e
Matt <matthewtmuir@gmail.com>
2021-05-09 23:12:11 -0700

    Created method that receives a string and returns a Phone object.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,37 @@ namespace RoboCollaboration
     {
         static void Main(string[] args)
         {
-            
+
+
+        }
+
+        public static Phone CreatePhoneObj(string str)
+        {
+            //break string into it's three components (name, number, phone type) and stores them in an array.
+            string[] PhoneNumberInfo = str.Split(',');
+
+            //Searches through each character of the string with the phone number. Stores each NUMBER found in number variable.
+            var holder = PhoneNumberInfo[1];
+            string number = "";
+
+            foreach (var item in holder)
+            {
+                if (Char.IsNumber(item))
+                {
+                    number += item;
+                }
+            }
+
+
+            //Does the actual Phone object creation
+            if (PhoneNumberInfo[2] == "1")
+            {
+                return new HomePhone(PhoneNumberInfo[0], number, PhoneNumberInfo[2]);
+            }
+            else
+            {
+                return new CellPhone(PhoneNumberInfo[0], number, PhoneNumberInfo[2]);
+            }
 
         }
     }
