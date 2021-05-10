5b726789020a378d88a93bb7b0daf70f55fd2daf
Austin Kautz <kautz1000@gmail.com>
2021-05-10 08:41:17 -0700

    populating object data

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace RoboCollaboration
     {
         static void Main(string[] args)
         {
-
+            PopulateObjectData();
 
         }
 
@@ -38,6 +38,19 @@ namespace RoboCollaboration
                 return new CellPhone(PhoneNumberInfo[0], number, PhoneNumberInfo[2]);
             }
 
+        }
+        public static void PopulateObjectData()
+        {
+            CreatePhoneObj("CompuTest,(303) 985-5060,1");
+            CreatePhoneObj("Curtis Manufacturing,(603) 532-4123,2");
+            CreatePhoneObj("Data Functions,(800) 876-2524,1");
+            CreatePhoneObj("Donnay Repair,(708) 397-3330,1");
+            CreatePhoneObj("ErgoNomic Inc,(360) 434-3894,1");
+            CreatePhoneObj("ErgoSource,(800) 969-4374,1");
+            CreatePhoneObj("Fox Bay Industries,(800) 874-8527,2");
+            CreatePhoneObj("Glare-Guard,(800) 545-6254,2");
+            CreatePhoneObj("Hazard Comm Specialists,(407) 783-6641,2");
+            CreatePhoneObj("Komfort Support,(714) 472-4409,2");
         }
     }
 }
