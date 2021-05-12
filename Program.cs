23779370f4749d384654329121f6e85482da68a5
Matt <matthewtmuir@gmail.com>
2021-05-12 08:09:35 -0700

    fixed PopulateData again

--- Program.cs
+++ Program.cs
@@ -44,16 +44,17 @@ namespace RoboCollaboration
         {
             Phone[] phoneBook = new Phone[10];
 
-            CreatePhoneObj("CompuTest,(303) 985-5060,1");
-            CreatePhoneObj("Curtis Manufacturing,(603) 532-4123,2");
-            CreatePhoneObj("Data Functions,(800) 876-2524,1");
-            CreatePhoneObj("Donnay Repair,(708) 397-3330,1");
-            CreatePhoneObj("ErgoNomic Inc,(360) 434-3894,1");
-            CreatePhoneObj("ErgoSource,(800) 969-4374,1");
-            CreatePhoneObj("Fox Bay Industries,(800) 874-8527,2");
-            CreatePhoneObj("Glare-Guard,(800) 545-6254,2");
-            CreatePhoneObj("Hazard Comm Specialists,(407) 783-6641,2");
-            CreatePhoneObj("Komfort Support,(714) 472-4409,2");
+            phoneBook[0] = CreatePhoneObj("CompuTest,(303) 985 - 5060, 1");
+            phoneBook[1] = CreatePhoneObj("Curtis Manufacturing,(603) 532-4123,2");
+            phoneBook[2] = CreatePhoneObj("Data Functions,(800) 876-2524,1");
+            phoneBook[3] = CreatePhoneObj("Donnay Repair,(708) 397-3330,1");
+            phoneBook[4] = CreatePhoneObj("ErgoNomic Inc,(360) 434-3894,1");
+            phoneBook[5] = CreatePhoneObj("ErgoSource,(800) 969-4374,1");
+            phoneBook[6] = CreatePhoneObj("Fox Bay Industries,(800) 874-8527,2");
+            phoneBook[7] = CreatePhoneObj("Glare-Guard,(800) 545-6254,2");
+            phoneBook[8] = CreatePhoneObj("Hazard Comm Specialists,(407) 783-6641,2");
+            phoneBook[9] = CreatePhoneObj("Komfort Support,(714) 472-4409,2");
+
 
             return phoneBook;
         }
